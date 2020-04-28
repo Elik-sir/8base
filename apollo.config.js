5efa68204e150e4fa5319bb4daf83fadd30cc54a
Eldar Kurmanaliev <ekurmanaliev50@gmail.com>
2020-04-28 15:05:41 +0300

    set app for start work

--- apollo.config.js
+++ apollo.config.js
@@ -2,10 +2,8 @@ module.exports = {
   client: {
     service: {
       name: '8base',
-      url: 'https://api.8base.com/cjrkt66qe000001ryzd3q4aiv',
+      url: 'https://api.8base.com/ck9i70nyc000207l873t41cn6',
     },
-    includes: [
-      "client/src/*.{ts,tsx,js,jsx}"
-    ]
+    includes: ['client/src/*.{ts,tsx,js,jsx}'],
   },
 };
